e4be73143348d66972db9e49a55821fd6463cc25
Camaro <cstcamaro@gmail.com>
2021-07-05 21:29:36 +0800

    Update tenpay_encrypt.js

--- tenpay_encrypt.js
+++ tenpay_encrypt.js
@@ -1,13 +1,11 @@
-var getCookie = function (name) {
-  var arr, reg = new RegExp("(^|; )" + name + "=([^;]*)(;|$)");
-  if (arr = document.cookie.match(reg)) {
-    return unescape(arr[2])
-  } else {
-    return ''
-  }
-}
+function getUrlParam(name) {
+      var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+      var r = window.location.search.substr(1).match(reg);
+      if (r != null) return unescape(r[2]); return null;
+}          
 
-var SKEY = getCookie("skey")
+var SKEY = getUrlParam("skey")
+var UIN = getUrlParam("uin")
 var vendor_lib = function (e) {
   function t(n) {
     if (r[n]) return r[n].exports;
