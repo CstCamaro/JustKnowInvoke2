1093364cd0d74020042363cd3a0301eae7eec43a
Camaro <cstcamaro@gmail.com>
2022-03-06 12:25:44 +0800

    Update hiddendemo.js

--- hiddendemo.js
+++ hiddendemo.js
@@ -1,3 +1,4 @@
+alert('foo');
 var parentNode = document.getElementsByClassName("ant-tree-child-tree ant-tree-child-tree-open")[0];
 parentNode.childNodes.forEach(function (item){
     let keyword = '回复1'
